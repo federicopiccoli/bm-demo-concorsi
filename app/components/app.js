368b2d1e7b3b4966bc484426efbd9eae4753345d
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-12 13:01:21 +0200

    Current language cookie storage

--- app/components/app.js
+++ app/components/app.js
@@ -10,16 +10,25 @@
  */
 angular
   .module('bmDemoConcorsiApp', [
+    'ngCookies',
     'ui.router', 
     'dsg.loadingCounter', 
     'pascalprecht.translate'
   ])
-  .controller('AppCtrl', function ($scope, $translate, AVAILABLE_LANGUAGES) {
-    $scope.availableLanguages = AVAILABLE_LANGUAGES;
-    $scope.currentLanguage = $translate.use();
+  .constant('LANG_COOKIE_KEY', 'lang_cookie')
+  .controller('AppCtrl', function ($scope, $cookies, $translate, AVAILABLE_LANGUAGES, LANG_COOKIE_KEY) {
     
     $scope.changeLanguage = function (lang) {
       $translate.use(lang);
       $scope.currentLanguage = lang;
+      $cookies.put(LANG_COOKIE_KEY, lang);
     };
+
+    var cookieLang = $cookies.get(LANG_COOKIE_KEY);
+    if (angular.isDefined(cookieLang)) {
+      $scope.changeLanguage(cookieLang);
+    }
+
+    $scope.availableLanguages = AVAILABLE_LANGUAGES;
+    $scope.currentLanguage = $translate.proposedLanguage();
   });
