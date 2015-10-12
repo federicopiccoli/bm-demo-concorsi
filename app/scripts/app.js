51ee4d074eec9b815419a381d6cc2229b8fa9e1e
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-12 09:42:37 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bmDemoConcorsiApp
+ * @description
+ * # bmDemoConcorsiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bmDemoConcorsiApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bmDemoConcorsiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bmDemoConcorsiApp
+ */
+angular.module('bmDemoConcorsiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
