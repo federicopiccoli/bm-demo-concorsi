4bd2a12b033fa7bda85d4fbaa2b0202cf0646b74
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-13 09:33:13 +0200

    Added sanitize startegy on ng-translate and fixed bug on lang selection

--- app/components/l10n.js
+++ app/components/l10n.js
@@ -4,6 +4,7 @@ angular.module('bmDemoConcorsiApp')
   .constant('AVAILABLE_LANGUAGES', ['it', 'en', 'sw'])
   .config(function ($translateProvider, AVAILABLE_LANGUAGES) {
     $translateProvider
+      .useSanitizeValueStrategy(null)
       .useStaticFilesLoader({
         prefix: '/l10n/',
         suffix: '.json'
